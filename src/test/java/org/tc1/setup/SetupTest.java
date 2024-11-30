5754fa62d1f7411d166010d91a1721e59c9d81fb
vmacena <macena.v@aluno.ifsp.edu.br>
2024-11-30 14:38:05 -0300

    test: testing and starting webDriverManager

---
+++ src/test/java/org/tc1/setup/SetupTest.java
@@ -0,0 +1,47 @@
+package org.tc1.setup;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SetupTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-web-security");
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    @DisplayName("Validate WebDriver Initialization")
+    public void testDriverInitialization() {
+        assertNotNull(
+                driver, "WebDriver was not initialized correctly.");
+    }
+}
