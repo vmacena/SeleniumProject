fa5ea5ebfa2fdd7b09a77bf524a98f3ce4c8b11a
m-brito <britomauricio2003@gmail.com>
2024-12-04 00:19:29 -0300

    test: add `SignInPage` for handling signin page interactions

---
+++ src/test/java/org/tc1/pages/SignInPage.java
@@ -0,0 +1,36 @@
+package org.tc1.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.tc1.utils.Utils;
+
+public class SignInPage {
+    private WebDriver driver;
+
+    public SignInPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private By loginModalButton = By.xpath("//*[@id=\"root\"]/div/nav/div[2]/button[1]");
+    private By emailInput = By.xpath("//*[@id=\"root\"]/div/nav/div[3]/div/form/label[1]/input");
+    private By passwordInput = By.xpath("//*[@id=\"root\"]/div/nav/div[3]/div/form/label[2]/input");
+    private By loginButton = By.xpath("//*[@id=\"root\"]/div/nav/div[3]/div/form/div/button[2]");
+    private By cancelButton = By.xpath("//*[@id=\"root\"]/div/nav/div[3]/div/form/div/button[1]");
+
+    public void clickLogInModalButton() {
+        Utils.clickElement(driver, loginModalButton);
+    }
+
+    public void fillForm(String email, String password) {
+        Utils.sendKeys(driver, emailInput, email);
+        Utils.sendKeys(driver, passwordInput, password);
+    }
+
+    public void submitForm() {
+        Utils.clickElement(driver, loginButton);
+    }
+
+    public void cancelForm() {
+        Utils.clickElement(driver, cancelButton);
+    }
+}
