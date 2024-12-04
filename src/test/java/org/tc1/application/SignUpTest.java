062359da2c5b1f457ffe47ff666878ae66cc1b89
gabrmanh <gmanhani@yahoo.com>
2024-12-04 18:26:50 -0300

    test: should not allow birthdates from the future

--- src/test/java/org/tc1/application/SignUpTest.java
+++ src/test/java/org/tc1/application/SignUpTest.java
@@ -114,6 +114,18 @@ public class SignUpTest extends SetupTest {
                         "123",
                         "123",
                         true
+                ),
+                Arguments.of(
+                        faker.name().firstName(),
+                        faker.internet().emailAddress(),
+                        faker.number().numberBetween(1, 2),
+                        dateFormat.format(faker.date().birthday(-200, -100)),
+                        1,
+                        String.valueOf(faker.number().numberBetween(10000, 999999)),
+                        faker.number().numberBetween(1, 3),
+                        "123",
+                        "123",
+                        true
                 )
         );
     }
